873de6ac2b6d830bff4a133892bad9d49b25b61a
zhangjie <zhangjieboot@gmail.com>
2019-05-31 22:27:26 +0800

    add

--- app/helpers/main_cases_helper.rb
+++ app/helpers/main_cases_helper.rb
@@ -1,2 +1,13 @@
 module MainCasesHelper
+    def department_collection
+        Department.all.map { |department| [department.name, department.id] }
+    end
+
+    def case_stages_collection
+        MainCase.case_stages.map { |key, value| [ MainCase::CASE_STAGE_MAP[key.to_sym], value ] }
+    end
+
+    def financial_stage_collection
+        MainCase.financial_stages.map { |key, value| [ MainCase::FINANCIAL_STAGE_MAP[key.to_sym], value ] }
+    end
 end
