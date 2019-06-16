c2a4b8ee5171f20595a64c475aa43b8a58971b54
zhangjie <zhangjieboot@gmail.com>
2019-06-16 16:34:53 +0800

    add

--- app/models/refund_order.rb
+++ app/models/refund_order.rb
@@ -4,7 +4,6 @@ class RefundOrder < ApplicationRecord
 	class << self
 		def take_department_user_id(payment_order)
 			payment_order.main_case.department.user_array.map{|e|[e.name,e.id]}
-			# [['小白','1'],['小明','2'],['小刚','3']]
-		end 
+		end
 	end
 end
