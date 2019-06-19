4eedaa7759073c17222e5ef9a3bcc828d9afbfd4
zhangjie <zhangjieboot@gmail.com>
2019-06-19 09:20:40 +0800

    添加委托单上的上传委托书

--- app/models/entrust_order.rb
+++ app/models/entrust_order.rb
@@ -4,6 +4,7 @@ class EntrustOrder < ApplicationRecord
 	belongs_to :user
 	belongs_to :organization
 	has_one :appraised_unit
+	has_one_attached :entrust_doc # 委托单下的委托书
 	accepts_nested_attributes_for :appraised_unit, reject_if: :all_blank, allow_destroy: true
 
 	validates :anyou, :presence => true
