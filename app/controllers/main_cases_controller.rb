45c93b3cafa9d9708295e5d49da25c62818dc0d7
guohang <hang.guo@classiclaw.com>
2019-07-17 22:19:10 +0800

    card 样式

--- app/controllers/main_cases_controller.rb
+++ app/controllers/main_cases_controller.rb
@@ -737,9 +737,7 @@ class MainCasesController < ApplicationController
         this_month_end = this_month_begin
         this_month_begin = this_month_begin - 1.month
         @data['month_mycase_count'] << {"count":department_cases.where(created_at: this_month_begin..this_month_end).count,"time":this_month_begin.strftime("%Y-%m")}
-      end 
-
-      # binding.pry
+      end
     end
 
   end
