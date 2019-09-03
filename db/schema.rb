d5da937abd8269557b798d5ff4993d4910cc2ade
zhangjie <zhangjieboot@gmail.com>
2019-09-03 09:23:06 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_014209) do
+ActiveRecord::Schema.define(version: 2019_09_02_140545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -188,6 +188,7 @@ ActiveRecord::Schema.define(version: 2019_08_12_014209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.text "matter"
     t.index ["organization_id"], name: "index_entrust_orders_on_organization_id"
     t.index ["user_id"], name: "index_entrust_orders_on_user_id"
   end
