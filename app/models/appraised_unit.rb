8c5ee27e48b35c467d18881de3be8c2e1a3de049
zhangjie <zhangjieboot@gmail.com>
2019-09-03 10:32:37 +0800

    remove set_birthday

--- app/models/appraised_unit.rb
+++ app/models/appraised_unit.rb
@@ -6,17 +6,17 @@ class AppraisedUnit < ApplicationRecord
   enum gender: [:male, :female]
   enum id_type: [:user_id, :other, :passport, :hong_kong_and_macau_pass]
 
-  before_save :set_birthday, if: -> { self.user? }
+  # before_save :set_birthday, if: -> { self.user? }
 
   UNIT_TYPE_MAP = { unit: '单位', user: '个人' }
   GENDER_MAP = { male: '男', female: '女' }
   ID_TYPE = { user_id: '身份证', passport: '护照', hong_kong_and_macau_pass: '港澳通行证', other: '其他' }
 
-  def set_birthday
-    if self.birthday.blank? && self.id_num.length == 18
-      self.birthday = self.id_num[6..13]
-    end
-  end
+  # def set_birthday
+  #   if self.birthday.blank? && self.id_num.length == 18
+  #     self.birthday = self.id_num[6..13]
+  #   end
+  # end
 
   class << self
     def unit_types_arr
